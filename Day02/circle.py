ddf8a8ee61fb701fd63839380dc811359873b64b
Osher Ben-Nun <osher@wismain.weizmann.ac.il>
2024-12-01 16:55:32 +0200

    add assignmment Day02

---
+++ Day02/circle.py
@@ -0,0 +1,5 @@
+import math
+
+radius = 4.5
+pi_num = math.pi
+print(f"The area of the circle is", round(pi_num*radius**2, 2), "and the circumference is", round(2*pi_num*radius, 2),".")
---
+++ Day02/rectangle.py
@@ -0,0 +1,5 @@
+width = 7 
+length = 77
+
+print(f"Area of the rectangle is: {width*length} and the perimeter is {2*(width+length)}")
+
